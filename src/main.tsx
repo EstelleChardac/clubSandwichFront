b7cbed48d243ad3681dd36166ac17eb32f7f81c8
Quentin GUERANDEL <quentin.guerandel@gmail.com>
2022-08-10 13:21:25 +0200

    preparing for final deployement

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <HashRouter>
+    <App />
+  </HashRouter>,
   document.getElementById('root'),
 );
